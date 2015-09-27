f5bde211ff0e738a0483cc8a4f8bd7074a7dbaa7
Katuva <wesley@twyman.io>
2015-09-27 21:46:05 +0100

    Upload validation and bug fix
    Added upload form validation, can no longer empty the limit views field
    Moment was throwing a warning as the datetime format returned from mysql
    wasn't good enough, so I just forced a conversion before passing it to
    moment

--- resources/assets/js/fromNowFilter.js
+++ resources/assets/js/fromNowFilter.js
@@ -1,3 +1,6 @@
 export default () => {
-    return (dateTime) => moment(dateTime).fromNow();
+    // moment throws a wobbler with the default datetime format returned,
+    // but we always know it'll be a valid datetime returned from the db,
+    // so we just force js's date parser to parse it first and bypass the warning.
+    return (dateTime) => moment(new Date(dateTime)).fromNow();
 }
